27300dad168eff1a6e427a6e84a9ba048d0defb0
Alberto Magariños Formigo <amagarinosf@uoc.edu>
2023-03-19 23:04:10 +0100

    Ejercicio 2-1

---
+++ PEC1_Ej2_1/script.js
@@ -0,0 +1,68 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+// Show input error message
+function showError (input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+// Show success outline
+function showSuccess (input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+// Get fieldname in Spanish using name, not id
+function getFieldName(input) {
+    return input.name;
+}
+
+// Check input length
+function checkLength (input, min, max) {
+    if (input.value.length < min) {
+        showError(input, `El campo ${getFieldName(input)} debe tener al menos ${min} caracteres`)
+    } else if (input.value.length > max) {
+        showError(input, `El campo ${getFieldName(input)} debe tener como máximo ${max} caracteres`)
+    } else {
+        showSuccess(input);
+    }
+}
+
+// Check email is valid
+function checkEmail(input) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(re.test(input.value.trim())){
+        showSuccess(input);
+    } else {
+        showError(input, 'El e-mail no es válido');
+    }
+}
+
+// Check passwords match
+function checkPasswordsMatch(input1, input2) {
+    if (input1.value !== input2.value) {
+        showError(input2, 'Este campo debe coincidir con el campo Contraseña');
+    }
+}
+
+// Event listeners
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    if (password2.value === '') {
+        showError(password2, 'La repetición de contraseña es necesaria');
+    } else {
+        showSuccess(password2);
+    }
+
+    checkLength(username, 3, 15);
+    checkEmail(email);
+    checkLength(password, 6, 25);
+    checkPasswordsMatch(password, password2);
+});
